64ca2a6937a34172ae7b38c2deb959e22bb93483
arjansunar <arjan.gahatrajsunar@gmail.com>
2022-08-13 15:37:31 +0545

    self avoiding walk

--- src/main.ts
+++ src/main.ts
@@ -2,36 +2,57 @@ import p5 from "p5";
 
 import "./style.css";
 
+const history = {} as { [key: string]: boolean };
+
 new p5((p5Instance: p5) => {
   const p = p5Instance;
 
-  const scale: number = 10;
+  const scale: number = 13;
 
   p.setup = function setup() {
     p.createCanvas(500, 500);
     p.background(0);
   };
 
-  let x = 250;
-  let y = 250;
+  let loc = p.createVector(250, 250);
+
   p.draw = function draw() {
+    let loc_prev = loc;
+    p.fill(220);
     let choice = randomWalk(p);
-
-    p.fill(220, 0, 0, 100);
-    p.ellipse(x, y, scale / 2);
-
-    x = x + choice.x * scale;
-    y = y + choice.y * scale;
+    loc.add(choice.mult(scale));
+    if (!isInHistory(loc)) {
+      p.ellipse(loc.x, loc.y, scale / 2);
+    } else {
+      loc = loc_prev;
+    }
+    addToHistory(loc);
   };
 }, document.getElementById("app")!);
 
 const randomWalk = (p5: p5) => {
   const choices = [
-    { x: 0, y: 1 }, // up
-    { x: 0, y: -1 }, // down
-    { x: -1, y: 0 }, // left
-    { x: 1, y: 0 }, // right
+    p5.createVector(0, 1), // up
+    p5.createVector(0, -1), // down
+    p5.createVector(-1, 0), // left
+    p5.createVector(1, 0), // right
   ];
   const choice = p5.floor(p5.random(4));
   return choices[choice];
+};
+
+const addToHistory = (loc: p5.Vector) => {
+  history[loc.x + "," + loc.y] = true;
+};
+
+const isInHistory = (loc: p5.Vector) => {
+  try {
+    let val = history[loc.x + "," + loc.y];
+    if (val) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    return false;
+  }
 };
